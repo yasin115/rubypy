424aafb4cf15a8d148e380c6bfe914bd7a93ce91
Yacn114 <yacn1214@gmail.com>
2025-07-22 03:26:51 +0330

    half db and pin and ban member

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 ## todo
 
 # user todo
-<li>add sqlite db</li>
 <li>add number to warn</li>
-<li>add remove user command</li>
 <li>add manual warn</li>
 <li>add wellcome and manual wellcome</li>
 <li>clean all message in group</li>
 <li>silent mode for user or group</li>
 <li>offline and online bot in group</li>
+
+## done
 <li>pin message in group</li>
+<li>add sqlite db</li>
+<li>add remove user command</li>
 
 # admin todo
 <li>list of groups</li>
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from rubpy import Client, filters
 from rubpy.types import Update
 import re
+# import sqlite3
+
+# conn = sqlite3.connect('data.db')
+# cursor = conn.cursor()
 
 bot = Client(name='rubpy')
 
@@ -9,22 +13,34 @@ async def updates(update: Update):
     admins = await bot.get_group_admin_members(update.object_guid)
     text = update.message.text
     name = await update.get_author(update.object_guid)
+    author_reply = await update.get_reply_author(update.object_guid,str(update.message.reply_to_message_id))
 
     # check admin access
     for admin in admins.in_chat_members:
         if admin.member_guid == update.message.author_object_guid:
                 
-            #change group name
-            if re.search(r'اسم جدید', text, re.IGNORECASE):
-                await bot.edit_group_info(update.object_guid,text[8:])
-                
+            # pin message
+            if 'پین' == text or 'pin' == text:
+                await update.pin(update.object_guid,update.message.reply_to_message_id)
+                await update.reply("done")
+            
+            # ban user    
+            if 'بن' == text or "سیک" == text or "ریمو" == text:
+                await update.ban_member(update.object_guid,author_reply.user.user_guid)
+                await update.reply(str("done"))
+            
+            # if text == "حذف پیام ها":
+            #     update.reply(update.get_messages(update.object_guid))
+                # update.delete_messages(update.object_guid,1000)
             break
         # check normal user            
         else:
         
             # anti link
             if re.search(r'(https?://|www\.)\S+\.(com|ir)|بیو', text, re.IGNORECASE):
-                await update.reply('اخطار ‍' + str(name['chat']['last_message']["author_title"]))
+                await update.reply('اخطار‍' 
+                                #    + str(name.chat.last_message.type)
+                                   )
                 await update.delete()
 
 

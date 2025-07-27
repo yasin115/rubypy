f8dbf5b4829fe7ae08d0d5ff1eff409876eb4914
Yacn114 <yacn1214@gmail.com>
2025-07-28 03:00:23 +0330

    use is_admin for base project and add wellcome messgae | v1

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 ## todo
 
 # user todo
+
 <li>add number to warn</li>
 <li>add manual warn</li>
-<li>add wellcome and manual wellcome</li>
-<li>clean all message in group</li>
+<li> and manual wellcome</li>
 <li>silent mode for user or group</li>
 <li>offline and online bot in group</li>
 
+# impossible
+
+<li>clean all message in group</li>
+
 ## done
+
+<li>add wellcome</li>
 <li>pin message in group</li>
 <li>add sqlite db</li>
 <li>add remove user command</li>
 
 # admin todo
+
 <li>list of groups</li>
 <li>send message for all group or a limited of numbers group</li>
--- main.py
+++ main.py
@@ -10,38 +10,38 @@ bot = Client(name='rubpy')
 
 @bot.on_message_updates(filters.text)
 async def updates(update: Update):
-    admins = await bot.get_group_admin_members(update.object_guid)
     text = update.message.text
     name = await update.get_author(update.object_guid)
-    author_reply = await update.get_reply_author(update.object_guid,str(update.message.reply_to_message_id))
-
-    # check admin access
-    for admin in admins.in_chat_members:
-        if admin.member_guid == update.message.author_object_guid:
-                
-            # pin message
-            if 'پین' == text or 'pin' == text:
-                await update.pin(update.object_guid,update.message.reply_to_message_id)
-                await update.reply("done")
-            
-            # ban user    
+    
+    # wellcome
+    if update.message.text == "یک عضو از طریق لینک به گروه افزوده شد." and update.message.type != "Text":
+        await update.reply("به گروه خوش اومدی")
+    if update.message.text == "یک عضو گروه را ترک کرد." and update.message.type != "Text":
+        await update.reply("سیکتیر کن عزیزم" )
+    
+    
+    # check admin
+    if await update.is_admin(update.object_guid,update.user_guid):
+        
+        # pin message
+        if 'پین' == text or 'pin' == text:
+            await update.pin(update.object_guid,update.message.reply_to_message_id)
+            await update.reply("done")
+        if update.reply_message_id != None:
+            # ban user
             if 'بن' == text or "سیک" == text or "ریمو" == text:
+                author_reply = await update.get_reply_author(update.object_guid,update.message.reply_to_message_id)
                 await update.ban_member(update.object_guid,author_reply.user.user_guid)
                 await update.reply(str("done"))
-            
-            # if text == "حذف پیام ها":
-            #     update.reply(update.get_messages(update.object_guid))
-                # update.delete_messages(update.object_guid,1000)
-            break
-        # check normal user            
-        else:
+    else:
         
-            # anti link
-            if re.search(r'(https?://|www\.)\S+\.(com|ir)|بیو', text, re.IGNORECASE):
-                await update.reply('اخطار‍' 
-                                #    + str(name.chat.last_message.type)
-                                   )
-                await update.delete()
-
+        # join group
+        #anti link
+        if re.search(r'(https?://|www\.)\S+\.(com|ir)|بیو', text, re.IGNORECASE):
+            await update.reply(' اخطار‍ ' 
+                                        + str(name.chat.last_message.author_title)
+                                        )
+            await update.delete()
+   
 
 bot.run()

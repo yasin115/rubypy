c6a150346392aad788ea16341b44c5750cc72de1
Yacn114 <yacn1214@gmail.com>
2025-07-30 01:56:06 +0330

    add help and amar count members

--- main.py
+++ main.py
@@ -27,18 +27,18 @@ async def updates(update: Update ):
     if await update.is_admin(update.object_guid,update.user_guid):
         
         # pin message
-        if 'پین' == text or 'pin' == text:
+        if 'پین' == text or 'pin' == text or text == "سنجاق":
             await update.pin(update.object_guid,update.message.reply_to_message_id)
-            await update.reply("done")
+            await update.reply("سنجاق شد")
         if update.reply_message_id != None:
             # ban user
-            if 'بن' == text or "سیک" == text or "ریمو" == text:
+            if 'بن' == text or "سیک" == text or "ریمو" == text :
                 author_reply = await update.get_reply_author(update.object_guid,update.message.reply_to_message_id)
                 await update.ban_member(update.object_guid,author_reply.user.user_guid)
                 first_name = name.chat.last_message.author_title or "کاربر"
 
-                text = f'<a href="rubika.ir/">{first_name}</a> بن شد.'
-                await update.reply(text, parse_mode='html')
+                text = f'{first_name} بن شد.'
+                await update.reply(text)
 
                 # await update.reply(f"{name.chat.last_message.author_title} بن شد.")
     else:
@@ -54,12 +54,27 @@ async def updates(update: Update ):
     ping_msg = ["pong","ربات فعال است ...","جوون"]
     #super admin
     if update.user_guid == "u0Gfirp0efb1e13736a9714fe315f443":
-        if text == "ping":
+        if text == "ping" or text == "ربات" or text == "پینگ":
             a = randint(0,2)
             await update.reply(ping_msg[a])
-        if update.message.text == "امار":
-            updates = await update.get_updates()
-            await update.reply(updates)
+        if text == "امار":
+            data = await bot.get_info(update.object_guid)
+            filter = data.group.count_members
+            await bot.send_message("u0Gfirp0efb1e13736a9714fe315f443",str(filter))
+
         if update.message.text == "ارسال به همه":
             await update.send_message(update.chat_id,"سلام دنیا!")
+    
+    #help 
+    if text == "راهنما":
+        await update.reply("""
+                     دستور های فعلی ربات :
+                     
+                     ربات / پینگ => فعال بودن ربات
+                     بن / سیک / ریمو => حذف کاربر
+                     سنجاق => سنجاق پیام ریپلای شده
+                    
+                    ربات رو باید ادمین کنید تا کار کنه در غیر اینصورت کار نخواهد کرد
+                    خوشامدگویی خودکار داره و لینک هارو پاک میکنه و اخطار میده
+                     """)
 bot.run()
